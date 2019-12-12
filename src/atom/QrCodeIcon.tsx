d74cba753bcdc67787a67a13432880868a664cd9
thedevluffy <thedevluffy@gmail.com>
2019-12-12 21:22:03 +0900

    feat: QrCodeIcon 을 구현한다.

---
+++ src/atom/QrCodeIcon.tsx
@@ -0,0 +1,9 @@
+import {CSSProperties} from "react";
+import * as React from 'react';
+import qrCode from './qr-code.svg';
+
+const qrCodeIconStyle: CSSProperties = {height: '24px', width: '24px'};
+
+const QrCodeIcon = () => <img alt="qrCode" src={qrCode} style={qrCodeIconStyle}/>;
+
+export default QrCodeIcon;
