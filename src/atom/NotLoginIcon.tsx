60e3408e540ae972031760bd5390c135e27008f0
thedevluffy <thedevluffy@gmail.com>
2019-12-12 21:51:47 +0900

    refactor: style 을 적용한다.

--- src/atom/NotLoginIcon.tsx
+++ src/atom/NotLoginIcon.tsx
@@ -2,6 +2,12 @@ import * as React from "react";
 
 import profile from './profile-user.svg';
 
-const NotLoginIcon = () => <img alt="notLogin" src={profile}/>;
+const notLoginIconStyle = (width: any, height: any) => ({
+    borderRadius: '50%',
+    height: `${height}`,
+    width: `${width}`,
+});
+
+const NotLoginIcon = ({width, height}: any) => <img alt="notLogin" src={profile} style={notLoginIconStyle(width, height)}/>;
 
 export default NotLoginIcon;
