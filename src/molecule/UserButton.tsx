57206888a887d0ec7ad397ceea2d99bfb6871832
thedevluffy <thedevluffy@gmail.com>
2019-12-13 14:31:49 +0900

    feat: molecule/UserButton 을 구현한다.

---
+++ src/molecule/UserButton.tsx
@@ -0,0 +1,23 @@
+import {CSSProperties} from "react";
+import * as React from "react";
+import UserIcon from "../atom/UserIcon";
+
+const iconStyle: CSSProperties = {
+    height: '55px',
+    width: '55px',
+};
+
+const UserButton = ({imgUrl}: any) => {
+    const userButtonClickHandler = (e: any) => {
+        e.preventDefault();
+        alert("user button clicked")
+    };
+
+    return (
+        <div className='userIcon' onClick={userButtonClickHandler} style={iconStyle}>
+            <UserIcon imgUrl={imgUrl} width='55px' height='55px'/>
+        </div>
+    );
+};
+
+export default UserButton;
