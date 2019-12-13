862999fcaaab7bd702a3934fbd640a5b7b79caa5
thedevluffy <thedevluffy@gmail.com>
2019-12-13 15:46:12 +0900

    feat: pages/Main 를 구현한다.

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import DefaultTemplate from "../templates/DefaultTemplate";
+
+const Main = () => {
+    return (
+        <DefaultTemplate title='대시보드' loggedIn={false}>
+            <div>
+                이것
+            </div>
+        </DefaultTemplate>
+    )
+};
+export default Main;
