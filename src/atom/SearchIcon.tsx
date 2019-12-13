8923e7dc41de2eb921c79194edf90fe77d78b050
thedevluffy <thedevluffy@gmail.com>
2019-12-13 20:46:29 +0900

    feat: atom/SearchIcon 을 구현한다.

---
+++ src/atom/SearchIcon.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import searchIcon from './search.svg'
+
+const SearchIcon = ({style}: any) => <img alt='user' src={searchIcon} style={style}/>;
+
+export default SearchIcon;
