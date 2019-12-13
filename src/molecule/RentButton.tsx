9818c373f69335d880a1fd985a764d14442828bc
thedevluffy <thedevluffy@gmail.com>
2019-12-13 14:33:26 +0900

    refactor: molecule/RentButton 을 리팩토링한다.

--- src/molecule/RentButton.tsx
+++ src/molecule/RentButton.tsx
@@ -1,24 +1,6 @@
+import * as React from "react";
 import {CSSProperties} from "react";
-import * as React from 'react';
-import styled from "styled-components";
 import QrCodeIcon from "../atom/QrCodeIcon";
-
-const Wrapper = styled.div`
-    background-color: rgba( 255, 255, 255, 0 );
-    bottom: 0;
-    height: 80px;
-    width: 100%;
-    position: absolute;
-`;
-
-const buttonStyle: CSSProperties = {
-    backgroundColor: 'black',
-    borderRadius: '10px',
-    boxShadow: '4px 3px 10px 0 rgba(0, 0, 0, 0.5)',
-    height: '50px',
-    marginLeft: '24px',
-    marginRight: '24px',
-};
 
 const textContainerStyle: CSSProperties = {
     alignItems: 'center',
@@ -41,22 +23,13 @@ const textStyle: CSSProperties = {
 };
 
 const RentButton = () => {
-    const onClickListener = (e: any) => {
-        e.preventDefault();
-        alert('qr rent');
-    };
-
     return (
-        <Wrapper>
-            <div className='qrCodeButton' style={buttonStyle} onClick={onClickListener}>
-                <div style={textContainerStyle}>
-                    <div style={textBoxStyle}>
-                        <QrCodeIcon/>
-                        <div style={textStyle}>대여하기</div>
-                    </div>
-                </div>
+        <div style={textContainerStyle}>
+            <div style={textBoxStyle}>
+                <QrCodeIcon/>
+                <div style={textStyle}>대여하기</div>
             </div>
-        </Wrapper>
+        </div>
     )
 };
 
