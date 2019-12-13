8209a307b19536f149fc7bc1f9a8d8401418a306
thedevluffy <thedevluffy@gmail.com>
2019-12-13 14:31:27 +0900

    refactor: molecule/ProfileButton 을 리팩토링한다.

--- src/molecule/ProfileButton.tsx
+++ src/molecule/ProfileButton.tsx
@@ -1,47 +1,21 @@
+import * as React from "react";
 import {CSSProperties} from "react";
-import * as React from 'react';
 import NotLoginIcon from '../atom/NotLoginIcon';
-import UserIcon from "../atom/UserIcon";
 
 const iconStyle: CSSProperties = {
     height: '55px',
     width: '55px',
 };
 
-const buttonStyle: CSSProperties = {
-    borderRadius: '50%',
-    boxShadow: '1px 1px 4px 0 rgba(0, 0, 0, 0.5)',
-    justifyContent: 'center',
-    marginTop: '8px',
-    width: 'fit-content',
-};
-
-const ProfileButton = ({loggedIn, imgUrl}: any) => {
+const ProfileButton = () => {
     const profileButtonClickHandler = (e: any) => {
         e.preventDefault();
         alert("profile button clicked")
     };
 
-    const userButtonClickHandler = (e: any) => {
-        e.preventDefalut();
-        alert("user button clicked")
-    };
-
-    const profileButton = (
+    return (
         <div className='profileIcon' onClick={profileButtonClickHandler} style={iconStyle}>
             <NotLoginIcon width='55px' height='55px'/>
-        </div>
-    );
-
-    const userButton = (
-        <div className='userIcon' onClick={userButtonClickHandler} style={iconStyle}>
-            <UserIcon imgUrl={imgUrl} width='55px' height='55px'/>
-        </div>
-    );
-
-    return (
-        <div className='profileButton' style={buttonStyle}>
-            {(loggedIn || imgUrl) ? userButton : profileButton}
         </div>
     );
 };
