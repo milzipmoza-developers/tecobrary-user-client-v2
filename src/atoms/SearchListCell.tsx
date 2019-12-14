ae41887fb539c553b8a4d7d19335263e1247282e
thedevluffy <thedevluffy@gmail.com>
2019-12-14 13:21:17 +0900

    feat: atoms/SearchListCell 을 구현한다.

---
+++ src/atoms/SearchListCell.tsx
@@ -0,0 +1,36 @@
+import {FontWeightProperty} from "csstype";
+import * as React from "react";
+
+interface IProps {
+    fontWeight: FontWeightProperty;
+    children?: string;
+    fontSize?: string;
+    width: string;
+}
+
+const tableCellStyle = (fontWeight: FontWeightProperty, width: string, fontSize?: string) => ({
+    display: 'table-cell',
+    fontSize: fontSize ? fontSize : '14px',
+    fontWeight,
+    height: '24px',
+    width,
+});
+
+const textBoxStyle = {
+    height: '24px',
+    lineHeight: '24px',
+    overflow: 'hidden',
+    width: '100%',
+};
+
+const SearchListCell = (props: IProps) => {
+    return (
+        <div style={tableCellStyle(props.fontWeight, props.width, props.fontSize)}>
+            <div style={textBoxStyle}>
+                {props.children}
+            </div>
+        </div>
+    )
+};
+
+export default SearchListCell;
