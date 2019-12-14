5470c399e0ffc5f8bd5e3cd734f5fbda81fc3c47
thedevluffy <thedevluffy@gmail.com>
2019-12-14 13:26:00 +0900

    feat: pages/Search 에 template/SearchListCard 를 추가한다.

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,16 +1,35 @@
 import * as React from "react";
-import TitledCard from "../organisms/TitledCard";
+import {useState} from "react";
 import DefaultTemplate from "../templates/DefaultTemplate";
 import SearchComponent from "../templates/SearchComponent";
+import SearchListCard from "../templates/SearchListCard";
+
+interface IBookProps {
+    id: string;
+    title: string;
+    author: string;
+    publisher: string;
+}
+
+const dummyBookList: IBookProps[] = [
+    // {id: '1', title: '객체 지향의 사실과 오해',  author: '조영호', publisher: '우아한'},
+    // {id: '2', title: 'JPA 프로그래밍',           author: '김영한', publisher: '우아한형'},
+    // {id: '8', title: 'RxJava 프로그래밍',        author: '유동환', publisher: '우아'},
+    // {id: '12', title: '양파껍질 학습법',         author: '박재성', publisher: '우아한형제'},
+    // {id: '14', title: '자바 8 인 액션',          author: '마리오 푸스코', publisher: '우아한형제들'},
+    // {id: '17', title: '배민다움',                author: '김봉진', publisher: '배민'},
+    // {id: '19', title: '스타트업이 일하는 방법',  author: '김범준', publisher: '배달의 민족'},
+    // {id: '30', title: 'WoowaDH 아시아 법인',     author: '그러게 말이다', publisher: '요기요'},
+    // {id: '45', title: '개발왕 루피 그는 누구인가', author: '권민철', publisher: '딜리버리 히어로 ㅋㅋㅋㅋㅋㅋ'},
+];
 
 const Search = () => {
+    const [bookList] = useState(dummyBookList);
+
     return (
         <DefaultTemplate title='도서 검색' loggedIn={true}>
             <SearchComponent isSearchPage={true}/>
-            <TitledCard>
-                {/* TODO: 목록 구현 */}
-                <div>1번</div>
-            </TitledCard>
+            <SearchListCard bookList={bookList}/>
         </DefaultTemplate>
     );
 };
