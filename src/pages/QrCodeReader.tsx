7e68cadeb72de66c89bd907cfff15f491e23909e
thedevluffy <thedevluffy@gmail.com>
2019-12-14 13:51:43 +0900

    feat: pages/QrCodeReader 를 구현한다.

---
+++ src/pages/QrCodeReader.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import DefaultTemplate from "../templates/DefaultTemplate";
+
+const QrCodeReader = () => {
+    return (
+        <DefaultTemplate title='대여하기' loggedIn={false}>
+            대여하기
+        </DefaultTemplate>
+    )
+};
+
+export default QrCodeReader;
