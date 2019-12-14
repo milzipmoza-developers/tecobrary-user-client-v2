6c5aa4092709a19227f2a1bff55ab2ed2e5a2ce3
thedevluffy <thedevluffy@gmail.com>
2019-12-14 13:22:48 +0900

    feat: organisms/SearchList 를 구현한다.

---
+++ src/organisms/SearchList.tsx
@@ -0,0 +1,49 @@
+import {CSSProperties, useState} from "react";
+import * as React from "react";
+import Divider from "../atoms/Divider";
+import SearchListElement from "../molecules/SearchListElement";
+
+interface IBookProps {
+    id: string;
+    title: string;
+    author: string;
+    publisher: string;
+}
+
+interface IProps {
+    children?: any;
+}
+
+const noContentStyle: CSSProperties = {
+    fontSize: '14px',
+    fontWeight: 'lighter',
+    textAlign: 'center'
+};
+
+const SearchList = ({children}: IProps) => {
+    const [books] = useState(children);
+
+    const searchListRowClick = (bookId: string) => {
+        alert('book id : ' + bookId);
+    };
+
+    const bookList = books.map((book: IBookProps, id: number) => (
+        <div>
+            {/* tslint:disable-next-line:jsx-no-lambda */}
+            <SearchListElement id={book.id} book={book} onClick={() => searchListRowClick(book.id)}/>
+            <Divider index={id} lastIndex={books.length}/>
+        </div>
+    ));
+
+    const noContent = (
+        <div style={noContentStyle}>검색 결과가 없습니다.</div>
+    );
+
+    return (
+        books.length !== 0 ?
+            bookList :
+            noContent
+    )
+};
+
+export default SearchList;
