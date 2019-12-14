581b991f2c5d0ca64fe8706a81abe24d08c73f1a
thedevluffy <thedevluffy@gmail.com>
2019-12-14 14:31:56 +0900

    feat: pages/QrCodeReader 에 qr code reader 를 추가한다.

--- src/pages/QrCodeReader.tsx
+++ src/pages/QrCodeReader.tsx
@@ -1,10 +1,27 @@
 import * as React from "react";
+import * as QrReader from 'react-qr-reader';
+import TitledCard from "../organisms/TitledCard";
 import DefaultTemplate from "../templates/DefaultTemplate";
 
 const QrCodeReader = () => {
+    const handleScan = (data: any) => {
+        if (data) {
+            // TODO: Request Rent
+            // tslint:disable-next-line:no-console
+            console.log(data);
+        }
+    };
+
+    const handleError = (error: Error) => {
+        // tslint:disable-next-line:no-console
+        console.error(error)
+    };
+
     return (
-        <DefaultTemplate title='대여하기' loggedIn={false}>
-            대여하기
+        <DefaultTemplate title='대여하기' loggedIn={false} visibleRentBtn='hidden'>
+            <TitledCard>
+                <QrReader onScan={handleScan} onError={handleError}/>
+            </TitledCard>
         </DefaultTemplate>
     )
 };
