7e3757f12e38b8e02570005943c27ad3b81492f5
thedevluffy <thedevluffy@gmail.com>
2019-12-15 15:16:43 +0900

    feat: templates/CardTextLinkButton 을 구현한다.

---
+++ src/templates/CardTextLinkButton.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {CSSProperties} from "react";
+import {colorProps, cursorProps} from "../atoms/types";
+import TextLinkButton from "../molecules/TextLinkButton";
+
+interface IProps {
+    to: string;
+    cursor: cursorProps;
+    color: colorProps;
+    children: string;
+}
+
+const buttonContainerStyle: CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '4px',
+    marginRight: '12px',
+    width: '100%',
+};
+
+const buttonLayerStyle: CSSProperties = {
+    alignSelf: 'flex-end',
+    padding: '4px'
+};
+
+const CardTextLinkButton = ({to, cursor, color, children}: IProps) => {
+    return (
+        <div style={buttonContainerStyle}>
+            <div style={buttonLayerStyle}>
+                <TextLinkButton to={to} color={color} cursor={cursor}>
+                    {children}
+                </TextLinkButton>
+            </div>
+        </div>
+    );
+};
+
+export default CardTextLinkButton;
