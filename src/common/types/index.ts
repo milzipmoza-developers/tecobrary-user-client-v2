ff1f1ee165d499d4a867176133d03953078e776e
thedevluffy <thedevluffy@gmail.com>
2019-12-15 22:10:46 +0900

    feat: color 를 추가한다.

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,2 +1,3 @@
 export type cursorProps = 'pointer' | 'initial' | undefined;
-export type colorProps = '#3E8BFF' | '#FF0000' | '#000000'
+export type colorProps = '#3E8BFF' | '#FF0000' | '#000000' | '#00d42a' | '#ffffff' | '#48d77f' | '#ff5d5f';
+export type textAlignProps = 'center' | 'initial' | 'inherit' | 'end' | undefined;
