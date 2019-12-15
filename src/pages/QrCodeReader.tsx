431d9ec6424ca619b2b4c7b28accd5289e32c5e2
thedevluffy <thedevluffy@gmail.com>
2019-12-15 22:12:33 +0900

    feat: pages/QrCodeReader 에 Dialog 를 추가한다.

--- src/pages/QrCodeReader.tsx
+++ src/pages/QrCodeReader.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import {useState} from "react";
 import * as QrReader from 'react-qr-reader';
-import DefaultTemplate from "../templates/DefaultTemplate";
+import {BORDER_DEFAULT_GRAY} from "../common/colors";
+import NoFooterTemplate from "../templates/NoFooterTemplate";
+import SimpleDialog from "../templates/SimpleDialog";
 import TitledCard from "../templates/TitledCard";
 
 const QrCodeReader = () => {
+    const [dialog, setDialog] = useState(true);
+
     const handleScan = (data: any) => {
         if (data) {
             // TODO: Request Rent
@@ -17,12 +22,40 @@ const QrCodeReader = () => {
         console.error(error)
     };
 
+    const tempScanButtonHandler = () => {
+        if (!dialog) {
+            setDialog(true);
+        } else {
+            setDialog(false);
+        }
+        // tslint:disable-next-line:no-console
+        console.log('set dialog true')
+    };
+
     return (
-        <DefaultTemplate title='대여하기' loggedIn={false} visibleRentBtn='hidden' profileIcon='visible'>
+        <NoFooterTemplate title='대여하기' loggedIn={false} profileIcon='visible'>
             <TitledCard>
-                <QrReader onScan={handleScan} onError={handleError}/>
+                <QrReader onScan={handleScan} onError={handleError} showViewFinder={false}
+                          style={{
+                              border: '1px solid',
+                              borderColor: BORDER_DEFAULT_GRAY,
+                              borderRadius: '10px',
+                              maxWidth: '450px',
+                              minWidth: '250px'
+                          }}/>
+                <div style={{width: '50px', height: '50px', fontSize: '20px', cursor: 'pointer'}}
+                     onClick={tempScanButtonHandler}>스캔
+                </div>
             </TitledCard>
-        </DefaultTemplate>
+            {dialog ?
+                <SimpleDialog
+                    title='확인하기'
+                    mode='success'
+                    confirmOnClick={tempScanButtonHandler}
+                    cancelOnClick={tempScanButtonHandler}>
+                    ㅇㅇ
+                </SimpleDialog> : null}
+        </NoFooterTemplate>
     )
 };
 
