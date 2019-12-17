55a96865aac721f8c7f0e87603b20c1666d70e55
thedevluffy <thedevluffy@gmail.com>
2019-12-17 13:39:07 +0900

    feat: pages/RentHistory 에 필요한 기능들을 구현한다.

--- src/pages/RentHistory.tsx
+++ src/pages/RentHistory.tsx
@@ -1,10 +1,23 @@
 import * as React from "react";
+import {useEffect, useState} from "react";
+import * as RentHistoryController from '../common/controller/RentHistoryController';
+import {RentHistoryProps} from "../organisms/renthistory/RentHistoryProps";
+import RentHistoryList from "../organisms/RentHistoryList";
 import DefaultTemplate from "../templates/DefaultTemplate";
 
 const RentHistory = () => {
+    const [rentHistories, setRentHistories] = useState<RentHistoryProps[]>([]);
+    // @ts-ignore
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        setRentHistories(RentHistoryController.findRentHistoriesByUserId(1, page, 10));
+    }, [page]);
+
     return (
         <DefaultTemplate title='대여 기록' loggedIn={true} profileIcon='visible'>
-            대여기록
+            <div style={{height: '3vh'}}/>
+            <RentHistoryList rentHistories={rentHistories}/>
         </DefaultTemplate>
     );
 };
