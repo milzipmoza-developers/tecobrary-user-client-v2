4a9cdfdea2399b46ea74e20e0504430f13f7baa9
thedevluffy <thedevluffy@gmail.com>
2019-12-18 22:30:02 +0900

    style: atoms/librarybook 의 css 속성을 변경한다.

--- src/atoms/librarybook/LibraryBookImage.tsx
+++ src/atoms/librarybook/LibraryBookImage.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
+import {BORDER_DEFAULT_GRAY} from "../../common/colors";
 
 interface IProps {
     image: string;
 }
 
 const LibraryBookImage = ({image}: IProps) => (
-    <div style={{flex: 1}}>
-        <img src={image}/>
+    <div style={{flex: 1, margin: '6px', borderRadius: '10px', border: `1px solid ${BORDER_DEFAULT_GRAY}`, display: 'flex', flexDirection: 'column'}}>
+        <img src={image} style={{width: '100%', borderRadius: '10px'}}/>
     </div>
 );
 
