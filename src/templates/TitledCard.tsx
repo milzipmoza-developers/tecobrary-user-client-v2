512d4870a5f06d5c3bfdcba576405bbf87709613
thedevluffy <thedevluffy@gmail.com>
2019-12-18 15:22:09 +0900

    feat: templates/TitledCard 에 size 를 설정할 수 있도록 변경한다.

--- src/templates/TitledCard.tsx
+++ src/templates/TitledCard.tsx
@@ -1,25 +1,27 @@
 import * as React from "react";
-import {CSSProperties} from "react";
 import DefaultCard from "../organisms/DefaultCard";
 
 interface IProps {
     title?: string,
     children?: any,
+    fontSize?: string;
     elevation?: boolean,
 }
 
-const titleStyle: CSSProperties = {
+const titleStyle = (fontSize?: string) => ({
     color: '#000000',
-    fontSize: '21px',
-    fontWeight: 'bold',
+    fontSize: fontSize ? fontSize : '21px',
+    fontWeight: 500,
     height: '32px',
-    paddingBottom: '20px',
-};
+    lineHeight: '32px',
+    marginBottom: '20px',
+    overflow: 'hidden',
+});
 
 const TitledCard = (props: IProps) => {
     return (
         <DefaultCard elevation={props.elevation === true}>
-            {props.title ? <div className='title' style={titleStyle}>{props.title}</div> : null}
+            {props.title ? <div className='title' style={titleStyle(props.fontSize)}>{props.title}</div> : null}
             {props.children}
         </DefaultCard>
     );
