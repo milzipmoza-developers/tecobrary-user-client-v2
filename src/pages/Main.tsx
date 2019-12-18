546378e7c99f945984aaace3d78d93a87c8d0b1d
thedevluffy <thedevluffy@gmail.com>
2019-12-18 20:58:01 +0900

    feat: pages/Main 카드에 내용을 추가한다.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import CardMessageHolder from "../atoms/CardMessageHolder";
+import {TEXT_BUTTON_BLUE} from "../common/colors";
+import CardTextButton from "../templates/CardTextButton";
 import DefaultTemplate from "../templates/DefaultTemplate";
 import SearchComponent from "../templates/SearchComponent";
 import TitledCard from "../templates/TitledCard";
@@ -10,9 +13,13 @@ const Main = () => {
                          imgUrl='https://avatars0.githubusercontent.com/u/52121827?s=460&v=4'>
             <SearchComponent isSearchPage={false}/>
             <TitledCard title='추천도서' elevation={true}>
-                추천 도서 목록
+                <CardMessageHolder>서비스 준비 중입니다.</CardMessageHolder>
+                <CardTextButton cursor='pointer' color={TEXT_BUTTON_BLUE} to='/recommends'>
+                    더보기
+                </CardTextButton>
             </TitledCard>
         </DefaultTemplate>
     )
 };
+
 export default Main;
