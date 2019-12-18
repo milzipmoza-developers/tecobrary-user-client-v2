732207f56d123040b3daa532f0ea91805b8dc035
thedevluffy <thedevluffy@gmail.com>
2019-12-18 22:31:35 +0900

    feat: atoms/SmallButton 을 구현한다.

---
+++ src/atoms/SmallButton.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+interface IProps {
+    children: string;
+    onClick: any;
+}
+
+const buttonStyle = {
+    backgroundColor: 'black',
+    borderRadius: '10px',
+    boxShadow: '4px 3px 10px 0 rgba(0, 0, 0, 0.5)',
+    color: 'white',
+    cursor: 'pointer',
+    height: '36px',
+    width: '48px',
+};
+const SmallButton = ({children, onClick}: IProps) => (
+    <div onClick={onClick} style={buttonStyle}>{children}</div>
+);
+
+export default SmallButton;
