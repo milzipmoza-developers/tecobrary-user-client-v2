64f45ff64ed6a990020a6fcc8d432f358bb4ed6d
thedevluffy <thedevluffy@gmail.com>
2019-12-25 22:05:36 +0900

    style: api 명세에 맞게 네이밍을 변경한다.

--- src/organisms/renthistory/IRentHistoryProps.ts
+++ src/organisms/renthistory/IRentHistoryProps.ts
@@ -2,7 +2,7 @@ export interface IRentHistoryProps {
     bookId: number;
     title: string;
     author: string;
-    createdAt: number;
-    deletedAt: number;
+    rentDate: number;
+    returnDate: number;
     likeStatus: boolean;
 }
--- src/organisms/renthistory/RentHistoryProps.ts
+++ src/organisms/renthistory/RentHistoryProps.ts
@@ -1,19 +1,19 @@
 import {IRentHistoryProps} from "./IRentHistoryProps";
 
-export class RentHistoryProps implements IRentHistoryProps{
+export class RentHistoryProps implements IRentHistoryProps {
     public bookId: number;
     public title: string;
     public author: string;
-    public createdAt: number;
-    public deletedAt: number;
+    public rentDate: number;
+    public returnDate: number;
     public likeStatus: boolean;
 
-    constructor(bookId: number, title: string, author: string, createdAt: number, deletedAt: number, likeStatus: boolean) {
+    constructor(bookId: number, title: string, author: string, rentDate: number, returnDate: number, likeStatus: boolean) {
         this.bookId = bookId;
         this.title = title;
         this.author = author;
-        this.createdAt = createdAt;
-        this.deletedAt = deletedAt;
+        this.rentDate = rentDate;
+        this.returnDate = returnDate;
         this.likeStatus = likeStatus;
     }
 }
