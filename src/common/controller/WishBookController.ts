ff694abe38a2daf761c3e1a9f148cb4c93c748e1
thedevluffy <thedevluffy@gmail.com>
2019-12-26 21:05:34 +0900

    feat: WishBookController 를 구현한다.

--- src/common/controller/WishBookController.ts
+++ src/common/controller/WishBookController.ts
@@ -2,15 +2,8 @@ import {WishBookDto} from "../classes/WishBookDto";
 import {create} from "../service/WishBookSerivce";
 
 const createWishBook = async (wishBookDto: WishBookDto) => {
-    try {
-        const response = await create(wishBookDto);
-        // tslint:disable-next-line:no-console
-        console.log(response.data);
-    } catch (e) {
-        // tslint:disable-next-line:no-console
-        console.log(e);
-        throw new Error(e.message);
-    }
+    const response = await create(wishBookDto);
+    return response.data;
 };
 
 export const wishBookController = {
