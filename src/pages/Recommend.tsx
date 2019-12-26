024ef1c29fa84cb358153d098950f7b72ebd878b
thedevluffy <thedevluffy@gmail.com>
2019-12-26 14:49:18 +0900

    feat: pages/Recommend 에 서비스 준비중을 추가한다.

--- src/pages/Recommend.tsx
+++ src/pages/Recommend.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import CardMessageHolder from "../atoms/CardMessageHolder";
 import DefaultTemplate from "../templates/DefaultTemplate";
 
 const Recommend = () => {
@@ -6,7 +7,7 @@ const Recommend = () => {
         <DefaultTemplate title='추천 도서' loggedIn={true}
                          profileIcon='visible'
                          imgUrl='https://avatars0.githubusercontent.com/u/52121827?s=460&v=4'>
-            추천 도서 목록
+            <CardMessageHolder>서비스 준비중 입니다.</CardMessageHolder>
         </DefaultTemplate>
     );
 };
