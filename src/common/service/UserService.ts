6658de1ee236a97c6aabcdc6b76706d96849632f
thedevluffy <thedevluffy@gmail.com>
2019-12-26 21:06:06 +0900

    style: UserService 의 / 를 제거한다.

--- src/common/service/UserService.ts
+++ src/common/service/UserService.ts
@@ -1,11 +1,11 @@
 import base from "./Api";
 
 export const updateUserName = (id: number, newName: string) => (
-    base().patch('/users', {id, newName})
+    base().patch('users', {id, newName})
 );
 
 export const authUser = (code: string) => (
-    base().get('/auth/user', {
+    base().get('auth/user', {
         params: {code}
     })
 );
