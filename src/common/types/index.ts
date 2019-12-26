f0f85d3482fea5f5aa5de6c4daf5d0ab866dcc42
thedevluffy <thedevluffy@gmail.com>
2019-12-26 14:49:34 +0900

    feat: types 에 authLevel 을 추가한다.

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -2,6 +2,7 @@ export type cursorProps = 'pointer' | 'initial' | undefined;
 export type colorProps = '#3E8BFF' | '#FF0000' | '#000000' | '#00d42a' | '#ffffff' | '#48d77f' | '#ff5d5f';
 export type textAlignProps = 'center' | 'initial' | 'inherit' | 'end' | undefined;
 export type dialogMode = 'error' | 'success' | 'warning';
+export type authLevel = 'NONE' | 'USER' | 'MANAGER' | 'KING';
 
 export * from './IBookInfo';
 export * from './ISerial';
